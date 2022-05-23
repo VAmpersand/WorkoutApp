29936f6e3aa4be2a326c27e774b519838131b658
Viktor Prikolota <49166976+VAmpersand@users.noreply.github.com>
2022-05-23 20:28:30 +0400

    Initial Commit

---
+++ WorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by Viktor Prikolota on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
